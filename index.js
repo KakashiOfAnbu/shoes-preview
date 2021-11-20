37022b2a7acb75e16f44701bc3aba3114230d703
Bui Duy Manh <manh.bd206155@sis.hust.edu.vn>
2021-11-20 19:48:12 +0700

    .gitignore is now working

--- index.js
+++
@@ -1,7 +0,0 @@
-const item = document.querySelector('.animated-content');
-const container = document.querySelector('.relative');
-console.log(item);
-
-container.addEventListener('mouseover', () => {
-    item.classList.add('animate-fade-in-up-reverse');
-});
